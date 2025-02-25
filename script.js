0999743b4c91eea61807c8d86642f7c43206733a
Your Name <you@example.com>
2025-02-25 09:21:13 -0500

    using string to concatinate the input value

--- script.js
+++ script.js
@@ -3,34 +3,48 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+  //1st  way create input array where you push each input value
+  // then quotients array to push each quotient value
+  //and finaly a remainder array to push each remainder
+  //then assign quotient to input to start again the loop
+  //finaly reverse the remainder array and call join() to
+  //convert array into string
 
-  if (input === 0) {
-    result.innerText = "0";
-    return;
-  }
+  // const inputs = [];
+  // const quotients = [];
+  // const remainders = [];
 
-  while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
+  // if (input === 0) {
+  //   result.innerText = "0";
+  //   return;
+  // }
 
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
-  }
+  // while (input > 0) {
+  //   const quotient = Math.floor(input / 2);
+  //   const remainder = input % 2;
 
-  console.log("Inputs: ", inputs);
-  console.log("Quotients: ", quotients);
-  console.log("Remainders: ", remainders);
+  //   inputs.push(input);
+  //   quotients.push(quotient);
+  //   remainders.push(remainder);
+  //   input = quotient;
+  // }
 
-  result.innerText = remainders.reverse().join("");
+  // console.log("Inputs: ", inputs);
+  // console.log("Quotients: ", quotients);
+  // console.log("Remainders: ", remainders);
+
+  // result.innerText = remainders.reverse().join("");
 
   //Refactor while loop
-  /**
- *  let binary = "";
+  //create an empty string assign it to a variable
+  // inside the while loop, append that variable to the result
+  // of remainder of input (input%2) and assign it to same var
+  // var = input%2 + var;
+  // divide input by 2 and assign it to same var holding input value
+  //input = input/2
+  //the display the result on screen;
+
+  let binary = "";
 
   if (input === 0) {
     binary = "0";
@@ -42,7 +56,8 @@ const decimalToBinary = (input) => {
   }
 
   result.innerText = binary;
- */
+
+  //3rd way using recursive;
 };
 
 const checkUserInput = () => {
